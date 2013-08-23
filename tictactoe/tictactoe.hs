3782de78526e1271c2c33f51931191ed3c35a8e1
Alexey Astafyev <av.astafyev@gmail.com>
2013-08-23 17:48:08 +0400

    the very first commit of TIC TAC TOE

---
+++ tictactoe/tictactoe.hs
@@ -0,0 +1,9 @@
+module Tictactoe where
+
+welcome_message = unlines [
+  "************* TIC TAC TOE ***************",
+  "*****************************************"
+  ]
+
+main = do
+  putStrLn welcome_message
